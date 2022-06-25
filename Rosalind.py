aca558d75d265ae249a9c14342acf303f6435508
Ekaterina Koptelova <76552692+KoptelovaEkaterina@users.noreply.github.com>
2022-06-26 02:24:02 +0300

    Update Rosalind.py

--- Rosalind.py
+++ Rosalind.py
@@ -1,5 +1,14 @@
-# Count letters 'A', 'C', 'G', 'T' in string of file rosalind_dna.txt
+# Counting letters 'A', 'C', 'G', 'T' in string of file rosalind_dna.txt
 
 with open("rosalind_dna.txt") as file:
     nucleotides = file.read()
     print(nucleotides.count('A'), nucleotides.count('C'), nucleotides.count('G'), nucleotides.count('T') )
+
+# Making RNA from DNA, replacing T by U
+
+with open("rosalind_rna.txt") as file:
+    nucleotides = file.read()
+    print(nucleotides.replace('T', 'U'))
+  
+
+
