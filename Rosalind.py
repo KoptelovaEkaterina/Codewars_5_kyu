252da2759c1f7dd1d07b01d8d0a2b2e1f826750d
Ekaterina Koptelova <76552692+KoptelovaEkaterina@users.noreply.github.com>
2022-06-27 17:25:41 +0300

    Update Rosalind.py

--- Rosalind.py
+++ Rosalind.py
@@ -9,6 +9,13 @@ with open("rosalind_dna.txt") as file:
 with open("rosalind_rna.txt") as file:
     nucleotides = file.read()
     print(nucleotides.replace('T', 'U'))
-  
+    
+# Reverse complement of DNA strand using Python
+    
+from Bio.Seq import Seq
 
+seq = Seq(nucleotides)
+
+print(seq.reverse_complement())
+  
 
