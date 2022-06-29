d2dbb3fe6b275d07784e49b16d3b42bdfd49e6a1
Ekaterina Koptelova <76552692+KoptelovaEkaterina@users.noreply.github.com>
2022-06-29 04:05:16 +0300

    Update Rosalind.py

--- Rosalind.py
+++ Rosalind.py
@@ -16,5 +16,34 @@ from Bio.Seq import Seq
 
 seq = Seq(nucleotides)
 print(seq.reverse_complement())
-  
+ 
+    
+# Select the sequence with the highest GC composition
+# answer from:
+# https://www.youtube.com/watch?v=mw4FSbDro0A
+# https://www.youtube.com/watch?v=7k9nCLrHipQ
 
+def readFile(filePath):
+    with open(filePath, 'r') as file:
+        return [l.strip() for l in file.readlines()] 
+
+def gc_content(seq):
+    return round(
+        ((seq.count('C') + seq.count('G')) / len(seq) * 100), 6) 
+
+FASTAFile = readFile("rosalind_gc.txt")
+FASTADict = {}
+FASTALabel = ""
+
+for line in FASTAFile:
+    if '>' in line:
+        FASTALabel = line 
+        FASTADict[FASTALabel] = ""
+    else:
+        FASTADict[FASTALabel] += line
+# print(FASTADict)
+
+RESULTDict = {key: gc_content(value) for (key, value) in FASTADict.items()}
+MaxGCKey = max(RESULTDict, key=RESULTDict.get)
+
+print(f'{MaxGCKey[1:]}\n{RESULTDict[MaxGCKey]}')
