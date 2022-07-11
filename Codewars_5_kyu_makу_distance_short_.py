3ab1ef862d565e5c5e5bf77706794ec2bcc43f73
Ekaterina Koptelova <76552692+KoptelovaEkaterina@users.noreply.github.com>
2022-07-11 18:54:05 +0300

    Update Codewars_5_kyu_makу_distance_short_.py

--- Codewars_5_kyu_makу_distance_short_.py
+++ Codewars_5_kyu_makу_distance_short_.py
@@ -11,3 +11,20 @@ def choose_best_sum(t, k, ls):
         a = sorted(c, key=lambda x: (abs(x - t), x))
         return a[0]
     return None
+
+
+def variance(numbers): 
+    # your code here
+    sum, variance = 0, 0
+    sum_list = 0
+    for nu in numbers:
+        sum_list += nu
+    len_num = int(len(numbers))
+    mean = sum_list/len_num
+    
+    for num in numbers:
+        print(num)
+        num = (num - mean)**2 / len(numbers)
+        variance += num
+    return variance
+    pass
