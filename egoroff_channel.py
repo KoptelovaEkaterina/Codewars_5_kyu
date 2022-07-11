822c221bf740b247f29cc0662410d982f727a8b3
Ekaterina Koptelova <76552692+KoptelovaEkaterina@users.noreply.github.com>
2022-07-11 18:50:12 +0300

    Update egoroff_channel.py

--- egoroff_channel.py
+++ egoroff_channel.py
@@ -180,24 +180,3 @@ def check_password(pas):
 
         
 
-        
-#https://stepik.org/lesson/701974/step/6?unit=702075
-
-class Graph:
-    LIMIT_Y = [0, 10]
-    new_list = []
-
-    def set_data(self, data):
-        self.data = data
-
-
-    def draw(self):
-        for num in self.data:
-            if num in range(Graph.LIMIT_Y[0],Graph.LIMIT_Y[1]+1):
-                Graph.new_list.append(num)
-        print(* Graph.new_list)
-
-
-graph_1 = Graph()
-graph_1.set_data([10, -5, 100, 20, 0, 80, 45, 2, 5, 7])
-graph_1.draw()        
